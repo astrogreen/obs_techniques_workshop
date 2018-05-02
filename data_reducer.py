e9a044997fc4bb1b7525113e04c7eedcefe8386d
Andy Green <andrew.green@aao.gov.au>
2018-05-02 12:21:45 +1000

    Observation classification.

--- data_reducer.py
+++ data_reducer.py
@@ -23,6 +23,12 @@ class SAMIObservation(object):
         self.raw_filename = raw_filename
         self.provenance_data = {}
 
+        with fits.open(self.raw_filename) as fits_data:
+
+            self.ndf_class = fits_data["STRUCT.MORE.NDF_CLASS"].data[0][0]
+
+            self.plate_id = fits_data[0].header["PLATEID"]
+
 
 class SAMIReductionGroup(object):
 
@@ -59,8 +65,46 @@ class SAMIReductionManager(object):
 
         self.reduction_groups = {}  # type: Dict[str, SAMIReductionGroup]
 
+    def all_observations(self):
+        all_obs = set()
+        all_obs.update(self.tramline_observations)
+        all_obs.update(self.arc_observations)
+        all_obs.update(self.flatfield_observations)
+        all_obs.update(self.science_observations)
+        return all_obs
+
     def import_new_observation(self, observation):
-        pass
+        # type: (SAMIObservation) -> None
+
+        if observation.plate_id not in self.reduction_groups:
+            self.reduction_groups[observation.plate_id] = SAMIReductionGroup(observation.plate_id, "sami1000R.idx")
+
+        reduction_group = self.reduction_groups[observation.plate_id]
+
+        # Classify observation based on NDF CLASS
+        if observation.ndf_class == "MFFFF":
+            self.tramline_observations.append(observation)
+
+            if reduction_group.tlm_observation is None:
+                reduction_group.tlm_observation = observation
+
+            self.flatfield_observations.append(observation)
+
+            if reduction_group.fiber_flat_observation is None:
+                reduction_group.fiber_flat_observation = observation
+
+        elif observation.ndf_class == "MFARC":
+            self.arc_observations.append(observation)
+
+            if reduction_group.arc_observation is None:
+                reduction_group.arc_observation = observation
+
+        elif observation.ndf_class == "MFOBJECT":
+            self.science_observations.append(observation)
+
+            if observation not in reduction_group.science_observation_list:
+                reduction_group.science_observation_list.append(observation)
+
 
     def reduce_all(self):
         pass
--- test_data_reducer.py
+++ test_data_reducer.py
@@ -4,12 +4,8 @@ from glob import glob
 
 import os
 import tempfile
-import shutil
-
-import tdfdr
 
 TEST_DATA_DIR = os.path.join(os.path.dirname(__file__), "../sami_example_data/150422/ccd_2/")
-ALL_DATA_DIR = os.path.join(os.path.dirname(__file__), "../sami_example_data/")
 
 from data_reducer import *
 
@@ -38,3 +34,32 @@ def test_pytest_not_capturing_fds(pytestconfig):
     # otherwise calls to "aaorun" seem to fail. This next test ensures that is the case.
     print("If this test fails, then you must run pytest with the option '--capture=sys'.")
     assert pytestconfig.getoption("capture") == "sys"
+
+
+
+
+def test_ndf_class_object():
+
+    sami_obs = SAMIObservation(TEST_DATA_DIR + "22apr20074.fits")
+
+    assert sami_obs.ndf_class == "MFFFF"
+
+
+def test_new_observation_add_and_classify(temporary_working_directory_with_data):
+
+    mngr = SAMIReductionManager()
+
+    for f in glob("*.fits"):
+        obs = SAMIObservation(f)
+
+        mngr.import_new_observation(obs)
+
+    assert len(mngr.science_observations) == 2
+    assert len(mngr.arc_observations) == 1
+    assert len(mngr.flatfield_observations) == 1
+    assert len(mngr.tramline_observations) == 1
+
+    assert len(mngr.all_observations()) == 4
+
+
+    assert "Y14SAR3_P005_12T056_15T080" in mngr.reduction_groups
